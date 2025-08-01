2763acbcd75cafbdcd48eac78a19dcb7f1a21e82
Caio Csermak <ccaiocsermak@gmail.com>
2025-08-01 09:47:48 -0300

    Feat: Criado model Filme e registro em /admin

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Filme(models.Model):
+    """Um modelo pra representar um filme que o usuário assistiu."""
+    titulo = models.CharField(max_length=200)
+    titulo = models.CharField(max_length=100)
+    ano_lancamento = models.IntegerField()
+    sinopse = models.TextField()
+    data_cadastro = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        """Retorna uma representação legível do título do filme."""
+        return self.titulo
+
